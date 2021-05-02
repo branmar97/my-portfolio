41b9063a99557a7f352cd9d57c409256ad151061
branmar97 <brandonmar97@gmail.com>
2021-05-02 19:56:46 -0400

    add nav links

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,36 @@
+import Link from 'next/link';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/portfolio">
+              <a>Portfolio</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
