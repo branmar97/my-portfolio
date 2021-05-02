1b18a210a0466720fa457c523e46e5994c31a625
branmar97 <brandonmar97@gmail.com>
2021-05-02 19:57:09 -0400

    add about page

---
+++ pages/about.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Brandon Marrero | About</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          About Me
+        </h1>
+      </main>
+    </div>
+  )
+}
