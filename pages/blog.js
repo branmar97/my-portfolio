81382f5234f0d3b536b2d918aaac1c5322c3076c
branmar97 <brandonmar97@gmail.com>
2021-05-02 19:57:22 -0400

    add blog page

---
+++ pages/blog.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Portfolio() {
+  return (
+    <div>
+      <Head>
+        <title>Brandon Marrero | Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Blog
+        </h1>
+      </main>
+    </div>
+  )
+}
