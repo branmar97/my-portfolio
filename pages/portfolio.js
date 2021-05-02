add47d50c52be3597b02046c972794693e58be5c
branmar97 <brandonmar97@gmail.com>
2021-05-02 19:57:31 -0400

    add portfolio page

---
+++ pages/portfolio.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Portfolio() {
+  return (
+    <div>
+      <Head>
+        <title>Brandon Marrero | Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Portfolio
+        </h1>
+      </main>
+    </div>
+  )
+}
