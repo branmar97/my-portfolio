45d5ed031196477dfd922af93eca86d321cdd3f2
branmar97 <brandonmar97@gmail.com>
2021-05-02 20:28:36 -0400

    render blog posts

--- pages/blog.js
+++ pages/blog.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link';
+import { blogPosts } from '../lib/data'
 
 export default function Portfolio() {
   return (
@@ -12,6 +14,15 @@ export default function Portfolio() {
         <h1>
           Blog
         </h1>
+        <div>
+            {blogPosts.map((item) => (
+                <div key={item.slug}>
+                    <div><Link href={`/blog/${item.slug}`}><a>{item.title}</a></Link></div>
+                    <div>{item.date.toString()}</div>
+                    <div>{item.content}</div>
+                </div>
+            ))}
+        </div>
       </main>
     </div>
   )
