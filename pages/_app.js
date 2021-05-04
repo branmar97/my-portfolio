529ab6a4a3fd8fa0939ae84bfc02072503309d81
branmar97 <brandonmar97@gmail.com>
2021-05-04 18:16:15 -0400

    add styling to nav

--- pages/_app.js
+++ pages/_app.js
@@ -3,31 +3,48 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/portfolio">
-              <a>Portfolio</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              <a>Blog</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto my-8 w-11/12">
+      <header>
+        <div className="flex justify-between my-4 mb-16">
+        
+          <Link href="/">
+            <a><h1 className="text-2xl font-bold text-center text-yellow-light">{"<Brandon />"}</h1></a>
+          </Link>
+
+          <nav>
+            <ul className="flex flex-row justify-center space-x-4 text-gray-light">
+              <li>
+                <span className="text-yellow-light">{"{"}</span>
+                <Link href="/">
+                  <a> Home </a>
+                </Link>
+                <span className="text-yellow-light">{"}"}</span>
+              </li>
+              <li>
+                <span className="text-yellow-light">{"{"}</span>
+                <Link href="/portfolio">
+                  <a> Portfolio </a>
+                </Link>
+                <span className="text-yellow-light">{"}"}</span>
+              </li>
+              <li>
+                <span className="text-yellow-light">{"{"}</span>
+                <Link href="/blog">
+                  <a> Blog </a>
+                </Link>
+                <span className="text-yellow-light">{"}"}</span>
+              </li>
+              <li>
+                <span className="text-yellow-light">{"{"}</span>
+                <Link href="/about">
+                  <a> About </a>
+                </Link>
+                <span className="text-yellow-light">{"}"}</span>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
       <Component {...pageProps} />
     </div>
   )
