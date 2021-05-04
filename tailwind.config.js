d5921b2076025d4c86268d83ae0d316fda51e876
branmar97 <brandonmar97@gmail.com>
2021-05-04 18:15:45 -0400

    added custom color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: {
+        light: '#282f6d',
+        DEFAULT: '#1fb6ff',
+        dark: '#11142e',
+      },
+      pink: {
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#c7d2ef',
+        lightest: '#f9fafc',
+      },
+      yellow: {
+        light: '#f6ba50'
+      }
+    },
     extend: {},
   },
   variants: {
