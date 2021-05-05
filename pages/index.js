82eb0b1d3633a29c86929e831961bb6dd2393c03
branmar97 <brandonmar97@gmail.com>
2021-05-05 13:31:13 -0400

    update text color

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,14 @@ export default function Home() {
         <title>Brandon Marrero</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
-      <div className='mx-auto text-left w-10/12 sm:w-6/12 md:w-8/12 space-y-4'>
+      <div className='mx-auto text-left w-10/12 sm:w-9/12 md:w-8/12 space-y-4'>
         <p className="text-yellow-light text-2xl">
           Hello, my name is
         </p>
-        <h1 className="text-gray-light text-4xl md:text-6xl">
+        <h1 className="text-off-white text-4xl sm:text-6xl">
           Brandon Marrero.
         </h1>
-        <h2 className="text-gray-light text-2xl md:text-4xl">
+        <h2 className="text-off-white text-2xl sm:text-4xl">
           {"<Software Engineer />"}
         </h2>
         <div>
@@ -28,8 +28,8 @@ export default function Home() {
             className="rounded-full"
           />
         </div>
-        <p className="text-gray-light w-96 sm:w-72 md:w-96 lg:w-96">
-          I'm building responsive web applications with clean and elegant code using HTML/CSS, Ruby on Rails, JavaScript, React and various other tools. I talk about my journey on Twitter and commit my code to Github.
+        <p className="text-off-white w-96">
+          I'm building responsive web applications with clean and elegant code using HTML/CSS, Ruby on Rails, JavaScript, React and other tools. I talk about my journey on Twitter and commit my code to Github.
         </p>
         <div className="space-x-4">
           <SocialIcon 
