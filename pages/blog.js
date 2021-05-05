c6ad926558a0b224dac9c8fbe09dfa9d53e6e51a
branmar97 <brandonmar97@gmail.com>
2021-05-05 16:28:21 -0400

    update grid columns

--- pages/blog.js
+++ pages/blog.js
@@ -16,13 +16,13 @@ export default function Portfolio() {
           My Blog
         </h1>
         <small>Posts ({blogPosts.length})</small>
-        <div className="mt-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
+        <div className="mt-6 grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-10 justify-items-center">
             {blogPosts.map((item) => (
-                <div className="mb-6 border border-off-white shadow p-5 rounded space-y-2" key={item.slug}>
+                <div className="mb-6 border border-off-white shadow hover:shadow-2xl transition duration-500 ease-in-out p-5 rounded space-y-2" key={item.slug}>
                     <div className="text-xl font-bold"><Link href={`/blog/${item.slug}`}><a>{item.title}</a></Link></div>
-                    <div>{format(parseISO(item.date), 'MMMM do, uuu')}</div>
-                    <div>{item.content}</div>
-                    <div><Link href={`/blog/${item.slug}`}><a><button className="bg-orange-dark hover:bg-orange-light text-off-white text-sm py-2 px-4 rounded mt-3">Read More</button></a></Link></div>
+                    <div className="text-gray-light text-sm">{format(parseISO(item.date), 'MMMM do, uuu')}</div>
+                    <div className="w-88">{item.content}</div>
+                    <div><Link href={`/blog/${item.slug}`}><a><button className="bg-orange-dark hover:bg-orange-light transition duration-300 ease-in-out text-off-white text-sm py-2 px-4 rounded mt-3">Read More</button></a></Link></div>
                 </div>
             ))}
         </div>
