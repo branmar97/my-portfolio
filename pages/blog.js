9f60d07146d3f86bb28ea97cc082f09b4d3b1901
branmar97 <brandonmar97@gmail.com>
2021-05-05 13:34:10 -0400

    add blog styling

--- pages/blog.js
+++ pages/blog.js
@@ -4,22 +4,23 @@ import { blogPosts } from '../lib/data'
 
 export default function Portfolio() {
   return (
-    <div>
+    <div className="mx-auto w-11/12">
       <Head>
         <title>Brandon Marrero | Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Blog
+      <main className="text-off-white">
+        <h1 className="text-4xl">
+          My Blog
         </h1>
-        <div>
+        <div className="mt-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center">
             {blogPosts.map((item) => (
-                <div key={item.slug}>
-                    <div><Link href={`/blog/${item.slug}`}><a>{item.title}</a></Link></div>
+                <div className="mb-6 border border-off-white shadow p-5 rounded space-y-2" key={item.slug}>
+                    <div className="text-xl font-bold"><Link href={`/blog/${item.slug}`}><a>{item.title}</a></Link></div>
                     <div>{item.date.toString()}</div>
                     <div>{item.content}</div>
+                    <div><Link href={`/blog/${item.slug}`}><a><button className="bg-orange-dark hover:bg-orange-light text-off-white text-sm py-2 px-4 rounded mt-3">Read More</button></a></Link></div>
                 </div>
             ))}
         </div>
