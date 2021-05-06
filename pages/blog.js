b8f4f310e332d58d80abb7deb7a15a2944f23f5f
branmar97 <brandonmar97@gmail.com>
2021-05-06 17:42:56 -0400

    Fix blog card styling

--- pages/blog.js
+++ pages/blog.js
@@ -5,7 +5,7 @@ import { getAllPosts } from '../lib/data'
 
 export default function Blog({ posts }) {
   return (
-    <div className="mx-auto w-11/12">
+    <div className="max-w-5xl mx-auto">
       <Head>
         <title>Brandon Marrero | Blog</title>
         {/* <link rel="icon" href="/favicon.ico" /> */}
@@ -16,12 +16,12 @@ export default function Blog({ posts }) {
           My Blog
         </h1>
         <small>Posts ({posts.length})</small>
-        <div className="mt-6 grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-10 justify-items-center">
+        <div className="mt-6 grid md:grid-cols-1 lg:grid-cols-2 gap-20 justify-items-center">
             {posts.map((item) => (
-                <div className="mb-6 border border-off-white shadow hover:shadow-2xl transition duration-500 ease-in-out p-5 rounded space-y-2" key={item.slug}>
+                <div className="bg-black bg-opacity-25 shadow hover:shadow-2xl transition duration-500 ease-in-out rounded space-y-4 text-center justify-center p-8 max-w-xl" key={item.slug}>
                     <div className="text-xl font-bold"><Link href={`/blog/${item.slug}`}><a>{item.title}</a></Link></div>
                     <div className="text-gray-light text-sm">{format(parseISO(item.date), 'MMMM do, uuu')}</div>
-                    <div className="w-88">{item.summary}</div>
+                    <div className="text-left">{item.summary}</div>
                     <div><Link href={`/blog/${item.slug}`}><a><button className="bg-orange-dark hover:bg-orange-light transition duration-300 ease-in-out text-off-white text-sm py-2 px-4 rounded mt-3">Read More</button></a></Link></div>
                 </div>
             ))}
