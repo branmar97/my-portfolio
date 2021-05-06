20cb43dd2c6598a087fd9f597c62140f37cafea2
branmar97 <brandonmar97@gmail.com>
2021-05-05 20:26:49 -0400

    format posts

--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Link from 'next/link';
-import { format, parseISO, add } from 'date-fns'
-import { blogPosts } from '../lib/data'
+import { format, parseISO } from 'date-fns'
+import { getAllPosts } from '../lib/data'
 
-export default function Portfolio() {
+export default function Blog({ posts }) {
   return (
     <div className="mx-auto w-11/12">
       <Head>
@@ -15,13 +15,13 @@ export default function Portfolio() {
         <h1 className="text-4xl">
           My Blog
         </h1>
-        <small>Posts ({blogPosts.length})</small>
+        <small>Posts ({posts.length})</small>
         <div className="mt-6 grid md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-10 justify-items-center">
-            {blogPosts.map((item) => (
+            {posts.map((item) => (
                 <div className="mb-6 border border-off-white shadow hover:shadow-2xl transition duration-500 ease-in-out p-5 rounded space-y-2" key={item.slug}>
                     <div className="text-xl font-bold"><Link href={`/blog/${item.slug}`}><a>{item.title}</a></Link></div>
                     <div className="text-gray-light text-sm">{format(parseISO(item.date), 'MMMM do, uuu')}</div>
-                    <div className="w-88">{item.content}</div>
+                    <div className="w-88">{item.summary}</div>
                     <div><Link href={`/blog/${item.slug}`}><a><button className="bg-orange-dark hover:bg-orange-light transition duration-300 ease-in-out text-off-white text-sm py-2 px-4 rounded mt-3">Read More</button></a></Link></div>
                 </div>
             ))}
@@ -29,4 +29,18 @@ export default function Portfolio() {
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts()
+  return {
+    props: {
+      posts: allPosts.map(({ data, summary, slug }) => ({
+        ...data,
+        date: data.date.toISOString(),
+        summary: data.summary,
+        slug,
+      }))
+    }
+  };
 }
